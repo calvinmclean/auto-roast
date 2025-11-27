5cbf7866132a7a9cbe614574a44f81ac6aeea0a2
Calvin McLean <calvinlmc@gmail.com>
2025-11-27 13:18:32 -0700

    Add CLI and TWChart connector

--- go.mod
+++ go.mod
@@ -3,12 +3,36 @@ module autoroast
 go 1.25.1
 
 require (
+	github.com/calvinmclean/babyapi v0.32.0
+	github.com/calvinmclean/twchart v0.0.0-20251005010049-60882216a9e4
 	go.bug.st/serial v1.6.4
-	golang.org/x/term v0.37.0
 	tinygo.org/x/drivers v0.33.0
 )
 
 require (
+	github.com/FZambia/sentinel v1.1.1 // indirect
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/creack/goselect v0.1.2 // indirect
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-chi/render v1.0.3 // indirect
+	github.com/go-echarts/go-echarts/v2 v2.5.4 // indirect
+	github.com/gomodule/redigo v1.9.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/invopop/jsonschema v0.13.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mark3labs/mcp-go v0.33.0 // indirect
+	github.com/rs/xid v1.5.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tarmac-project/hord v0.6.0 // indirect
+	github.com/tarmac-project/hord/drivers/hashmap v0.6.0 // indirect
+	github.com/tarmac-project/hord/drivers/redis v0.6.0 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 	golang.org/x/sys v0.38.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ twchart/client.go
@@ -0,0 +1,106 @@
+package twchart
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/calvinmclean/babyapi"
+	"github.com/calvinmclean/twchart"
+)
+
+type Probes []twchart.Probe
+
+type Client struct {
+	client    *babyapi.Client[*session]
+	sessionID string
+}
+
+type session struct {
+	babyapi.DefaultResource
+	Session twchart.Session
+}
+
+func NewClient(addr string) Client {
+	client := babyapi.NewClient[*session](addr, "/sessions")
+	return Client{client: client}
+}
+
+func (c *Client) CreateSession(ctx context.Context, beanName string, probes Probes) (string, error) {
+	resp, err := c.client.Post(ctx, &session{
+		Session: twchart.Session{
+			Name:   beanName,
+			Date:   time.Now(),
+			Probes: []twchart.Probe(probes),
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	c.sessionID = resp.Data.GetID()
+
+	return resp.Data.GetID(), nil
+}
+
+func (c Client) SetStartTime(ctx context.Context, startTime time.Time) error {
+	_, err := c.client.Patch(ctx, c.sessionID, &session{Session: twchart.Session{
+		StartTime: startTime,
+	}})
+	return err
+}
+
+func (c Client) AddEvent(ctx context.Context, note string, now time.Time) error {
+	e := twchart.Event{Note: note, Time: now}
+
+	url, _ := c.client.URL(c.sessionID)
+	url += "/add-event"
+
+	return c.makeRequest(ctx, url, e)
+}
+
+func (c Client) AddStage(ctx context.Context, name string, now time.Time) error {
+	s := twchart.Stage{Name: name, Start: now}
+
+	url, _ := c.client.URL(c.sessionID)
+	url += "/add-stage"
+
+	return c.makeRequest(ctx, url, s)
+}
+
+func (c Client) Finish(ctx context.Context) error {
+	url, _ := c.client.URL(c.sessionID)
+	url += "/finish"
+
+	return c.makeRequest(ctx, url, nil)
+}
+
+func (c Client) makeRequest(ctx context.Context, url string, body any) error {
+	var bodyReader io.Reader = http.NoBody
+	if body != nil {
+		bodyBytes, err := json.Marshal(body)
+		if err != nil {
+			return fmt.Errorf("error encoding body: %w", err)
+		}
+
+		bodyReader = bytes.NewReader(bodyBytes)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bodyReader)
+	if err != nil {
+		return fmt.Errorf("error creating request: %w", err)
+	}
+	resp, err := c.client.MakeGenericRequest(req, nil)
+	if err != nil {
+		return fmt.Errorf("error making request: %w", err)
+	}
+	if resp.Response.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d, response: %v", resp.Response.StatusCode, resp.Body)
+	}
+
+	return nil
+}
